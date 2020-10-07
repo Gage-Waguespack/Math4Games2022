b39189c8262e1e331eaa99aec2657a4324d63952
Gage Waguespack <gagewaguespack@gmail.com>
2020-10-07 17:02:12 -0500

    added a scene, game, and actor class

---
+++ MathForGames/Actor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MathForGames
+{
+    class Actor
+    {
+        public void start()
+        {
+
+        }
+
+        public void update()
+        {
+
+        }
+
+        public void Draw()
+        {
+
+        }
+
+        public void End()
+        {
+
+        }
+
+    }
+}
