1f274fcf9d5c81adea570354203866d6d178fb98
Gage <gagewaguespack@gmail.com>
2020-10-12 17:02:08 -0500

    Added A way to move to the right in the console while pressing 'D'

--- MathForGames/Game.cs
+++ MathForGames/Game.cs
@@ -2,37 +2,55 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Text;
+using System.Threading;
 
 namespace MathForGames
 {
     class Game
     {
         private static bool _gameOver = false;
-
+        private Scene _scene;
+        private Actor _actor;
         //Static function used to set game over without an instance of game.
         public static void SetGameOver(bool value)
         {
             _gameOver = value;
         }
 
+        //Return whether or not the specefied ConsoleKey is pressed
+        public static bool CheckKey(ConsoleKey key)
+        {
+            if (Console.KeyAvailable)
+            {
+                if (Console.ReadKey(true).Key == key)
+                {
+                    return true;
+                }
+            }
+
+            return false;
+        }
 
         //Called when the game begins. Use this for initialization.
         public void Start()
         {
-
+            _scene = new Scene();
+            Actor actor = new Actor();
+            _scene.AddActor(actor);
         }
 
 
         //Called every frame.
         public void Update()
         {
-
+            _scene.Update();
         }
 
         //Used to display objects and other info on the screen.
         public void Draw()
         {
-
+            Console.Clear();
+            _scene.Draw();
         }
 
 
@@ -52,6 +70,9 @@ namespace MathForGames
             {
                 Update();
                 Draw();
+                while (Console.KeyAvailable) 
+                    Console.ReadKey(true);
+                Thread.Sleep(50);
             }
 
             End();
